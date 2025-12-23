d53c638ba6c1a1dceee62864e562c3ff2694757b
almlkghazy538-cmd <almlkghazy538@gmail.com>
2025-12-23 23:20:37 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8436742877:AAHmlmOKY2iQCGoOt004ruq09tZGderDGMQ")
+    ADMIN_ID = int(os.getenv("ADMIN_ID", 6130994941))
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///bot_database.db")
+    
+    # إعدادات النقاط
+    POINTS_PER_REFERRAL = 5
+    DAILY_GIFT_POINTS = 3
+    POINTS_PER_CHANNEL_SUB = 2
+    MIN_POINTS_FOR_FUNDING = 25
+    POINTS_PER_MEMBER = 25  # عدد النقاط لكل عضو
+    
+    # إعدادات السحب
+    MAX_MEMBERS_PER_REQUEST = 50
+    ADD_MEMBERS_DELAY = 1  # ثانية بين كل إضافة
